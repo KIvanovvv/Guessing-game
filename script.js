c3824c0da7fc3deb9db373462a1af589cf6793f5
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-08-13 17:37:05 +0100

    Add code
    Adding "Check' button logic.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 'use strict';
-const number = Math.trunc(Math.random() * 20) + 1;
-document.querySelector(`.number`).textContent = number;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector(`.number`).textContent = secretNumber;
 
 document.querySelector(`.check`).addEventListener(`click`, function () {
   const guess = Number(document.querySelector(`.guess`).value);
@@ -8,5 +8,11 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
 
   if (!guess) {
     document.querySelector(`.message`).textContent = `⛔ No number!`;
+  } else if (guess === secretNumber) {
+    document.querySelector(`.message`).textContent = `🎉 Correct number!`;
+  } else if (guess > secretNumber) {
+    document.querySelector(`.message`).textContent = `📈 Too high!`;
+  } else if (guess < secretNumber) {
+    document.querySelector(`.message`).textContent = `📉 Too low!`;
   }
 });
