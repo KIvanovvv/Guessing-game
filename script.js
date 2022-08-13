b24a90679a7eed41db9a7b303dcf433f6259b16f
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-08-13 18:01:34 +0100

    Add code
    Adding more "Check" button logic and "High score" counter logic.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 'use strict';
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector(`.number`).textContent = secretNumber;
 
 document.querySelector(`.check`).addEventListener(`click`, function () {
@@ -11,8 +12,22 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
   } else if (guess === secretNumber) {
     document.querySelector(`.message`).textContent = `🎉 Correct number!`;
   } else if (guess > secretNumber) {
-    document.querySelector(`.message`).textContent = `📈 Too high!`;
+    if (score > 1) {
+      score--;
+      document.querySelector(`.score`).textContent = score;
+      document.querySelector(`.message`).textContent = `📈 Too high!`;
+    } else {
+      document.querySelector(`.message`).textContent = `💥 You lost the game!`;
+      document.querySelector(`.score`).textContent = 0;
+    }
   } else if (guess < secretNumber) {
-    document.querySelector(`.message`).textContent = `📉 Too low!`;
+    if (score > 1) {
+      score--;
+      document.querySelector(`.score`).textContent = score;
+      document.querySelector(`.message`).textContent = `📈 Too low!`;
+    } else {
+      document.querySelector(`.message`).textContent = `💥 You lost the game!`;
+      document.querySelector(`.score`).textContent = 0;
+    }
   }
 });
