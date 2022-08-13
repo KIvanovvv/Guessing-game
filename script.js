1b985c6d4e4a19339c38ac7ad6d4f91b165206e7
Krasimir Ivanov <k.ivanov29@yahoo.com>
2022-08-13 19:29:44 +0100

    Add code
    Adding "high score" tracker and building reset feature on "Again!" button.

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 'use strict';
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector(`.number`).textContent = secretNumber;
+let highScore = 0;
 
 document.querySelector(`.check`).addEventListener(`click`, function () {
   const guess = Number(document.querySelector(`.guess`).value);
-  console.log(guess, typeof guess);
   //When there is no input
   if (!guess) {
     document.querySelector(`.message`).textContent = `⛔ No number!`;
@@ -14,6 +13,13 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
     document.querySelector(`.message`).textContent = `🎉 Correct number!`;
     document.querySelector(`body`).style.backgroundColor = `#60b347`;
     document.querySelector(`.number`).style.width = `30rem`;
+    document.querySelector(`.number`).textContent = secretNumber;
+    highScore = score;
+    if (Number(document.querySelector(`.highscore`).textContent) < highScore) {
+      document.querySelector(`.highscore`).textContent = highScore;
+    }
+
+    console.log(highScore);
     //When guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
@@ -35,4 +41,15 @@ document.querySelector(`.check`).addEventListener(`click`, function () {
       document.querySelector(`.score`).textContent = 0;
     }
   }
+});
+
+document.querySelector(`.again`).addEventListener(`click`, function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector(`body`).style.backgroundColor = `#222`;
+  score = 20;
+  document.querySelector(`.score`).textContent = score;
+  document.querySelector(`.number`).style.width = `15rem`;
+  document.querySelector(`.number`).textContent = `?`;
+  document.querySelector(`.guess`).value = '';
+  document.querySelector(`.message`).textContent = `Start guessing...`;
 });
